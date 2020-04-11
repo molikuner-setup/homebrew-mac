1618d69896ef14565fd9c092a5f1c3178402b35a
molikuner <molikuner@gmail.com>
2020-04-11 21:36:35 +0200

    Update duplicacy version
    ~ duplicacy @ 2.5.0

--- Formula/duplicacy.rb
+++ Formula/duplicacy.rb
@@ -1,9 +1,9 @@
 class Duplicacy < Formula
-  version "2.4.0"
+  version "2.5.0"
   desc "A new generation cloud backup tool"
   homepage "https://duplicacy.com"
   url "https://github.com/gilbertchen/duplicacy/releases/download/v#{version}/duplicacy_osx_x64_#{version}"
-  sha256 "1b6797c086b6196c30fe1593bd2f40ff552b0bcac29bf7b2c3c25c23d9293cf2"
+  sha256 "3deeecbb513cdc4ca72e5492dd1e0bb76a91ab0dc9e82771cee3dc6cd480bf56"
 
   bottle :unneeded
 
